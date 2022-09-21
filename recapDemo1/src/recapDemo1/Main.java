ad7454b00402f8742e32248569861e068d6d966c
avcio <avcio@10.242.40.197>
2022-09-21 16:34:27 +0300

    JavaProje-8

---
+++ recapDemo1/src/recapDemo1/Main.java
@@ -0,0 +1,27 @@
+package recapDemo1;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 20;
+		int sayi2 = 25;
+		int sayi3 = 2;
+		int enBuyuk = sayi1;
+		
+	if(enBuyuk<sayi2){
+		enBuyuk = sayi2;
+		System.out.println(sayi2);
+	}
+	
+	if(enBuyuk<sayi3){
+		enBuyuk = sayi3;
+		System.out.println(sayi3);
+	}
+		
+		
+
+	}
+
+}
